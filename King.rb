ac6c1bea42b80d6cee896c369cb68da59b74701b
Gitman <t.tuck303+github@gmail.com>
2015-09-16 13:51:22 -0500

    Adds King and Queen acceptable move methods (untested)

--- King.rb
+++ King.rb
@@ -8,4 +8,12 @@ class King < Piece
 		@symbol = assign_symbol(@team, @type)
 	end
 	
+
+	def acceptable_move?(x_differential, y_differential)
+		if x_differential.abs > 1 || y_differential.abs > 1
+			return false
+		else
+			return true
+		end
+	end
 end
--- Queen.rb
+++ Queen.rb
@@ -7,5 +7,18 @@ class Queen < Piece
 		@type = :queen
 		@symbol = assign_symbol(@team, @type)
 	end
+
+	def acceptable_move?(x_differential, y_differential)
+		if x_differential == 0
+			return true
+		elsif y_differential == 0
+			return true
+		elsif x_differential.abs == y_differential.abs
+			return true
+		else
+			return false
+		end
+	end
+	
 	
 end
