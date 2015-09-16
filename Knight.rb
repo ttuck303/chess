a163709a662222f20ecbaf1bf552e93356b1ee25
Gitman <t.tuck303+github@gmail.com>
2015-09-16 13:48:25 -0500

    Adds knight acceptable move check.

--- Knight.rb
+++ Knight.rb
@@ -8,6 +8,16 @@ class Knight < Piece
 		@symbol = assign_symbol(@team, @type)
 	end
 
+	def acceptable_move?(x_differential, y_differential) 
+		if x_differential.abs == 2 && y_differential.abs == 1
+			return true
+		elsif x_differential.abs == 1 && y_differential.abs == 2
+			return true
+		else
+			return false
+		end
+	end
+
 	
 	
 end
